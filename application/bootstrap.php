e7677c17bebc18375c206014ae913157eca9d107
Oleg Grigoryev <olegg@taxer.ua>
2017-02-05 19:44:14 +0200

    Добавил отображение ошибок и контент-тип utf-8

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+header("Content-Type: text/html; charset=utf-8");
+
 include 'core/model.php';
 include 'core/view.php';
 include 'core/controller.php';
 include 'core/route.php';
 Route::start();
-
-?>
